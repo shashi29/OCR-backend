cc300afc98156e1898560d8d1da8b3620b93a6e7
Shashi Raj <shashi.raj30@yahoo.in>
2024-10-16 21:04:37 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from app.api.routes import document, collection
 from app.core.logging import setup_logging
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*", "http://localhost:3000"]#,"https://prospero-two.vercel.app","http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Setup logging
 setup_logging()
